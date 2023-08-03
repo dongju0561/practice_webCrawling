2f6fe25385fe71fd3ace2edf81582d8a4f70d742
DongjuPark <pdj95044@gmail.com>
2023-08-03 14:09:05 +0900

    trying to get detail contents

--- main/gptCode.py
+++ main/gptCode.py
@@ -9,6 +9,31 @@ file_path = "/Users/DJ/Desktop/gitRes/webCrawling/main/crawling_result.txt"
 div = " | "
 row_data = "| "
 
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.stack.pop()
+        else:
+            return None
+
+    def peek(self):
+        if not self.is_empty():
+            return self.stack[-1]
+        else:
+            return None
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    def size(self):
+        return len(self.stack)
+
 def init_write(file_path):
     div_line = "--------------------------------------------\n"
 
@@ -53,26 +78,48 @@ for url in page_url_list:
         i_url = row.find_element(By.TAG_NAME, 'a').get_attribute('href')
 
         #상세 게시물 접속
+        """
+        코딩 플랜
+        상세 게시물의 내용은 태그들간 계층구조로 구성되어 있으며 높은 레벨부터 순서대로 텍스트들을 concat 작업 진행
+        """
         driver.get(i_url)
-        ps = driver.find_elements(By.TAG_NAME, 'p')
+        s = Stack()
+        ps = driver.find_elements(By.CLASS_NAME, 'boardview-con')
         for p in ps:
-            spans = p.find_elements(By.TAG_NAME, 'span')
-            for span in spans:
-                try: #span 내부 text가 strong태그로 감싸져 있는 경우 예외처리
-                    element = span.find_element(By.TAG_NAME,"strong")
-                except NoSuchElementException:
-                    pass
-                print(span.text)
+            l1s = p.find_elements(By.XPATH, './/*') #p태그 내부에 있는 모든 span태그들을 가져옴 레벨과 상관없이 그냥 다 가져옴
+            for l1 in l1s:
+                if(l1.text != ""):
+                    s.push(l1.text)
+                    print(len(s.stack))
+                    # print(s.stack)
+                    l2s = l1.find_elements(By.XPATH, './/*')
+                    for e in l2s:
+                        print(e.tag_name)
+                    for l2 in l2s:
+                        if(l2.text != ""):
+                            s.push(l2.text)
+                            l3s = l2.find_elements(By.XPATH, './/*')
+                            for l3 in l3s:
+                                if(l3.text != ""):
+                                    s.push(l3.text)
+                                    l2s = l3.find_elements(By.XPATH, './/*')
+            print(s.stack)
+            # for span in spans:
+            #     print(span.text)
+            #     try: #span 내부 text가 strong태그로 감싸져 있는 경우 예외처리
+            #         strong = span.find_element(By.TAG_NAME,"strong")
+            #     except NoSuchElementException:
+            #         pass
+        print("---------------------------------------------------------")
         driver.back()
         #상세 게시물 접속 끝
 
         for col in cols:
             current_class=col.get_attribute('class')
             if current_class == 'num' or current_class == '' or current_class == 'day' or current_class == 'writer dotdot' or current_class == 'view': #클래스 이름을 가지고 태그ul 안에 데이터들 중에 원하는 데이터를 골라 수집
-                print(col.text)
+                # print(col.text)
                 row_data += col.text
                 row_data += div
-        print("---------------------------------------------------------")
         
         try:
             with open(file_path, 'a') as file:
@@ -81,7 +128,6 @@ for url in page_url_list:
                 row_data = ""
                 file.writelines("\n")
                 
-            print("Add row")
         except IOError:
             print("Adding row failed")    
 
